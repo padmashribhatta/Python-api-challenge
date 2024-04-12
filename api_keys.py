ba560de99505a3ff21dc9c97a9f279575128a71b
padmashribhatta <padmashribhatta18@yahoo.com>
2024-04-11 20:27:42 -0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "587b7c30c5e7058d01edc715fecc56d6"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d58ece17c27a4ced9b6bf3af3b02ca21"
